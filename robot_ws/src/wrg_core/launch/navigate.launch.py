0c27d6ce438f890503eb06eaf118befaf94afc8e
mannaja <manman7852078520@gmail.com>
2024-07-30 22:09:32 +0700

    fix navigation

--- robot_ws/src/wrg_core/launch/navigate.launch.py
+++ robot_ws/src/wrg_core/launch/navigate.launch.py
@@ -1,19 +1,3 @@
-# Copyright 2019 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Author: Darby Lim
-
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -53,16 +37,15 @@ def generate_launch_description():
         'nav2_default_view.rviz'
     )
 
-    # Define nodes and include launch files
     navigate_node = Node(
         package='wrg_core',
         executable='navigate.py',
         name='navigate_node',
-        # output='screen',
+        output='screen',
         on_exit=[
             ExecuteProcess(
                 cmd=['killall', 'navigate.py'],
-                # output='screen'
+                output='screen'
             )
         ]
     )
@@ -73,19 +56,16 @@ def generate_launch_description():
             default_value=map_dir,
             description='Full path to map file to load'
         ),
-
         DeclareLaunchArgument(
             'params_file',
             default_value=param_dir,
             description='Full path to param file to load'
         ),
-
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use simulation (Gazebo) clock if true'
         ),
-
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(nav2_launch_file_dir, 'bringup_launch.py')
@@ -96,7 +76,6 @@ def generate_launch_description():
                 'params_file': param_dir
             }.items(),
         ),
-
         # Node(
         #     package='rviz2',
         #     executable='rviz2',
@@ -111,7 +90,6 @@ def generate_launch_description():
         #         )
         #     ]
         # ),
-        
         navigate_node,
-        # gazebo_launch,
     ])
+
